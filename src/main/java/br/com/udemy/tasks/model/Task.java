2ffd6d2bf38caa47ac6cb658489dcd8da1606ef7
Juliano Carneiro <julianoomc@gmail.com>
2024-07-16 11:21:33 -0300

    implementa padrão Builder

--- src/main/java/br/com/udemy/tasks/model/Task.java
+++ src/main/java/br/com/udemy/tasks/model/Task.java
@@ -12,6 +12,16 @@ public class Task {
 
     private TaskState state;
 
+    public Task() {
+    }
+
+    public Task(Builder builder) {
+        this.title = builder.title;
+        this.description = builder.description;
+        this.priority = builder.priority;
+        this.state = builder.state;
+    }
+
     public Task newTask() {
         TaskService.taskList.add(this);
         return this;
@@ -47,5 +57,58 @@ public class Task {
 
     public void setState(TaskState state) {
         this.state = state;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static Builder builderFrom(Task task) {
+        return new Builder(task);
+    }
+
+    public static class Builder {
+
+        private String title;
+
+        private String description;
+
+        private int priority;
+
+        private TaskState state;
+
+        public Builder() {
+        }
+
+        public Builder(Task task) {
+            this.title = task.title;
+            this.description = task.description;
+            this.priority = task.priority;
+            this.state = task.state;
+        }
+
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder withPriority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder withState(TaskState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
     }
 }
