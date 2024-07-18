e6fcc96c11a743d26dd76e19250a3e9bd066b48c
Juliano Carneiro <julianoomc@gmail.com>
2024-07-18 10:01:32 -0300

    implementa testes unitários de TaskController

---
+++ src/test/java/br/com/udemy/tasks/controller/TaskControllerTest.java
@@ -0,0 +1,82 @@
+package br.com.udemy.tasks.controller;
+
+import br.com.udemy.tasks.controller.converter.TaskDTOConverter;
+import br.com.udemy.tasks.controller.dto.TaskDTO;
+import br.com.udemy.tasks.model.Task;
+import br.com.udemy.tasks.service.TaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class TaskControllerTest {
+
+    @InjectMocks
+    private TaskController controller;
+
+    @Mock
+    private TaskService service;
+
+    @Mock
+    private TaskDTOConverter converter;
+
+    @Test
+    public void controller_mustReturnOk_whenGetPaginatedSuccessfully2() {
+        Task task = new Task();
+        Page<Task> page = new PageImpl<>(Collections.singletonList(task), PageRequest.of(0, 1), 1);
+
+        when(service.findPaginated(any(), anyInt(), anyInt())).thenReturn(page);
+
+        WebTestClient client = WebTestClient.bindToController(controller).build();
+
+        client.get()
+                .uri("/task")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Task.class);
+    }
+
+    @Test
+    public void controller_mustReturnOk_whenSavedSuccessfully() {
+        when(service.insert(any())).thenReturn(Mono.just(new Task()));
+        when(converter.convert(any(Task.class))).thenReturn(new TaskDTO());
+
+        WebTestClient client = WebTestClient.bindToController(controller).build();
+
+        client.post()
+                .uri("/task")
+                .bodyValue(new TaskDTO())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(TaskDTO.class);
+    }
+
+    @Test
+    public void controller_mustReturnNoContent_whenDeletedSuccessfully() {
+        String taskId = "any-id";
+
+        when(service.deleteById(any())).thenReturn(Mono.empty());
+
+        WebTestClient client = WebTestClient.bindToController(controller).build();
+
+        client.delete()
+                .uri("/task/" + taskId)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+}
